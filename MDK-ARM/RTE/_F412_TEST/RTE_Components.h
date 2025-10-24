caa3c36d28e4c093af9d180345fe87deac678b2a
NewNoob2002 <guptianci@gmail.com>
2025-10-24 19:15:36 +0800

    first

---
+++ MDK-ARM/RTE/_F412_TEST/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'F412_TEST' 
+ * Target:  'F412_TEST' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
